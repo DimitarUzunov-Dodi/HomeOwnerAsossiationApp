ccc79151412654a9fbc5e7d0c08c59b63dae3e4e
Faizel Mangroe <F.mangroe@student.tudelft.nl>
2022-12-21 05:10:51 +0100

    Add castVote endpoint and implement the method in the service
    - Create castRuleVotingVote endpoint in VotingController.
    - Implement castRuleVote method in VotingService.
    - Add RuleVoteRequestModel.
    - Refactor castVote to castElectionVote.

--- voting-microservice/src/main/java/nl/tudelft/sem/template/voting/models/ElectionVoteRequestModel.java
+++ voting-microservice/src/main/java/nl/tudelft/sem/template/voting/models/ElectionVoteRequestModel.java
@@ -3,10 +3,10 @@ package nl.tudelft.sem.template.voting.models;
 import lombok.Data;
 
 /**
- * Model representing a request with associationId.
+ * Model representing a request to cast a vote in an election.
  */
 @Data
-public class VoteRequestModel {
+public class ElectionVoteRequestModel {
     private int voterId;
     private int associationId;
     private int candidateId;
