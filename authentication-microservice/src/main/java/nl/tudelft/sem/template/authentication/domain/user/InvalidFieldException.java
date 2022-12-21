8fc4c55d52c96d2fa0160d3eb010c069acfe6c23
Albert Sandu <a.a.sandu@student.tudelft.nl>
2022-12-21 13:07:30 +0200

    fix javadoc

--- authentication-microservice/src/main/java/nl/tudelft/sem/template/authentication/domain/user/InvalidFieldException.java
+++ authentication-microservice/src/main/java/nl/tudelft/sem/template/authentication/domain/user/InvalidFieldException.java
@@ -1,7 +1,7 @@
 package nl.tudelft.sem.template.authentication.domain.user;
 
 /**
- * Exception to indicate the UserID is invalid.
+ * Exception to indicate the field is invalid.
  */
 public class InvalidFieldException extends Exception {
     static final long serialVersionUID = -3387516993334229948L;
