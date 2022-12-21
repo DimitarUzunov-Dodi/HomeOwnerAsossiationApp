0cf3dddeee865bfb2ecf2e9eb6fcd403b26e5a2b
Tianchen Qu <T.QU@student.tudelft.nl>
2022-12-21 16:37:06 +0100

    addition check ups for adding membership

--- association-microservice/src/main/java/nl/tudelft/sem/template/association/domain/membership/MembershipService.java
+++ association-microservice/src/main/java/nl/tudelft/sem/template/association/domain/membership/MembershipService.java
@@ -3,18 +3,30 @@ package nl.tudelft.sem.template.association.domain.membership;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import nl.tudelft.sem.template.association.domain.association.AssociationRepository;
+import nl.tudelft.sem.template.association.domain.user.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MembershipService {
     private final transient MembershipRepository membershipRepository;
 
-    /**constructor.
+    private final transient AssociationRepository associationRepository;
+
+    private final transient UserRepository userRepository;
+
+    /**
+     * constructor.
      *
-     * @param membershipRepository the repo
+     * @param membershipRepository  membership repo
+     * @param associationRepository association repo
+     * @param userRepository user repo
      */
-    public MembershipService(MembershipRepository membershipRepository) {
+    public MembershipService(MembershipRepository membershipRepository,
+                             AssociationRepository associationRepository, UserRepository userRepository) {
         this.membershipRepository = membershipRepository;
+        this.associationRepository = associationRepository;
+        this.userRepository = userRepository;
     }
 
     /**getter.
@@ -65,9 +77,16 @@ public class MembershipService {
      *
      * @return if successfully joined or not, new membership is not council as default
      */
-    public boolean addMembership(String userId, int assoId, Address address, Date joinDate) throws FieldNoNullException {
+    public boolean addMembership(String userId, int assoId, Address address, Date joinDate)
+            throws FieldNoNullException, IllegalArgumentException {
         if (address == null || joinDate == null) {
             throw new FieldNoNullException();
+        }
+        if (associationRepository.findById(assoId).isEmpty()) {
+            throw new IllegalArgumentException("No such association!");
+        }
+        if (!userRepository.existsByUserId(userId)) {
+            throw new IllegalArgumentException("No such user!");
         }
         if (membershipRepository.existsByUserIdAndAssociationId(userId, assoId)) {
             return false;
