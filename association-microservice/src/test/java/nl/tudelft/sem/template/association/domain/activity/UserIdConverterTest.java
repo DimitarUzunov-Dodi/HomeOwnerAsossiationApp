d9ac52d21e675b8b133549c9da089833aa252d6a
Dimitar Uzunov <D.S.Uzunov-1@student.tudelft.nl>
2022-12-22 21:17:43 +0100

    Chekstyle fix last I swear

--- association-microservice/src/test/java/nl/tudelft/sem/template/association/domain/activity/UserIdConverterTest.java
+++ association-microservice/src/test/java/nl/tudelft/sem/template/association/domain/activity/UserIdConverterTest.java
@@ -2,11 +2,8 @@ package nl.tudelft.sem.template.association.domain.activity;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
